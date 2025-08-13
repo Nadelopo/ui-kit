520c934241eff8de8b654a298f217f28980118c4
Nadelopo <mistrmorsikk@mail.ru>
2025-08-14 00:20:59 +0400

    init

---
+++ eslint.config.ts
@@ -0,0 +1,34 @@
+import { globalIgnores } from 'eslint/config'
+import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import pluginVue from 'eslint-plugin-vue'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginVueScopedCss from 'eslint-plugin-vue-scoped-css' // 👈
+
+// To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
+// import { configureVueProject } from '@vue/eslint-config-typescript'
+// configureVueProject({ scriptLangs: ['ts', 'tsx'] })
+// More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
+
+export default defineConfigWithVueTs(
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{ts,mts,tsx,vue}'],
+  },
+
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
+  pluginVue.configs['flat/recommended'],
+  vueTsConfigs.recommended,
+  skipFormatting,
+
+  {
+    plugins: {
+      'vue-scoped-css': pluginVueScopedCss,
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue-scoped-css/enforce-style-type': ['error', { allows: ['module'] }],
+      '@typescript-eslint/ban-ts-comment': 'off',
+    },
+  },
+)
